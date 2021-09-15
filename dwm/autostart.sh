b5549889fd0aff2d960219fdec8ccbfb4e0dfe20
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-09-15 17:42:25 -0300

    addeed keepassxc to autostart script

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -9,7 +9,7 @@ function run {
 
 # Render on multiple monitors, change depending on you xrarandr -q output
 run "xrandr --auto --output HDMI-1-0 --mode 1920x1080 --rate 60  --right-of eDP-1"
-run "kmix"
+#run "kmix"
 run "nm-applet"
 run "xfce4-power-manager"
 #run "blueberry-tray"
@@ -22,4 +22,4 @@ run slstatus &
 run "nitrogen --restore"
 run "wmname LG3D"
 run "unclutter"
-
+run keepassxc &
